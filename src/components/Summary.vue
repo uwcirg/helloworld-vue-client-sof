4e981c8c90ff300ae2c74aa0d71456c0c1be4a96
Amy Chen <achen2401@gmail.com>
2022-09-30 11:09:19 -0700

    add example to return to dashboard url (#3)
    * add example to return to dashboard url
    
    * Update Summary.vue
    
    Co-authored-by: Amy Chen <amysbubble@Amys-MacBook-Pro-2.local>

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -2,46 +2,59 @@
   <div id="interventionElement" v-if="ready">
     <h2 class="font-weight-bold text-h5 mb-4 mt-2" v-text="title"></h2>
     <v-card elevation="0">
-        <h4>Patient Info</h4>
-        <table>
-          <tr>
-            <td class="mr-2">Name</td>
-            <td v-text="summary.Patient.Name"></td>
-          </tr>
-           <tr>
-            <td class="mr-2">Gender</td>
-            <td v-text="summary.Patient.Gender"></td>
-          </tr>
-           <tr>
-            <td class="mr-2">Age</td>
-            <td v-text="summary.Patient.Age"></td>
-          </tr>
-        </table>
+      <h4>Patient Info</h4>
+      <table>
+        <tr>
+          <td class="mr-2">Name</td>
+          <td v-text="summary.Patient.Name"></td>
+        </tr>
+        <tr>
+          <td class="mr-2">Gender</td>
+          <td v-text="summary.Patient.Gender"></td>
+        </tr>
+        <tr>
+          <td class="mr-2">Age</td>
+          <td v-text="summary.Patient.Age"></td>
+        </tr>
+      </table>
     </v-card>
+    <!-- example UI for returning to (f)EMR URL -->
+    <div v-if="getReturnURL()" class="mt-2">
+      <v-btn :href="getReturnURL()">Return to Patient List</v-btn>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      summary: [Array, Object],
-      title: String
+import { fetchEnvData, getEnv } from "../util/util";
+export default {
+  props: {
+    summary: [Array, Object],
+    title: String,
+  },
+  watch: {
+    summary: function () {
+      this.ready = true;
     },
-    watch: {
-      'summary': function() {
-        this.ready = true;
-      }
+  },
+  data() {
+    return {
+      ready: false, // Indicate we are not yet ready for the component to render
+    };
+  },
+  created() {
+    fetchEnvData();
+  },
+  methods: {
+    hasResponses() {
+      return this.summary;
     },
-    data() {
-      return {
-        ready: false // Indicate we are not yet ready for the component to render
-      };
+    getReturnURL() {
+      return getEnv("VUE_APP_DASHBOARD_URL") + "/clear_session";
     },
-    methods: {
-      hasResponses () {
-        return this.summary;
-      }
-    }
-  };
+  },
+};
 </script>
-<style>/*component specific css here*/</style>
+<style>
+/*component specific css here*/
+</style>
--- src/util/util.js
+++ src/util/util.js
@@ -23,6 +23,66 @@ export function getFHIRResourcePaths(patientId) {
   });
 }
 
+export function fetchEnvData() {
+  if (window["appConfig"] && Object.keys(window["appConfig"]).length) {
+    console.log("Window config variables added. ");
+    return;
+  }
+  const setConfig = function () {
+    if (!xhr.readyState === xhr.DONE) {
+      return;
+    }
+    if (xhr.status !== 200) {
+      console.log("Request failed! ");
+      return;
+    }
+    var envObj;
+    try {
+      envObj = JSON.parse(xhr.responseText);
+    } catch (e) {
+      console.log("Error parsing response text into json ", e);
+    }
+    window["appConfig"] = {};
+    //assign window process env variables for access by app
+    //won't be overridden when Node initializing env variables
+    for (var key in envObj) {
+      if (!window["appConfig"][key]) {
+        window["appConfig"][key] = envObj[key];
+      }
+    }
+  };
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/env.json", false);
+  xhr.onreadystatechange = function () {
+    //in the event of a communication error (such as the server going down),
+    //or error happens when parsing data
+    //an exception will be thrown in the onreadystatechange method when accessing the response properties, e.g. status.
+    try {
+      setConfig();
+    } catch (e) {
+      console.log("Caught exception " + e);
+    }
+  };
+  try {
+    xhr.send();
+  } catch (e) {
+    console.log("Request failed to send.  Error: ", e);
+  }
+  xhr.ontimeout = function (e) {
+    // XMLHttpRequest timed out.
+    console.log("request to fetch env.json file timed out ", e);
+  };
+}
+
+export function getEnv(key) {
+  //window application global variables
+  if (window["appConfig"] && window["appConfig"][key])
+    return window["appConfig"][key];
+  const envDefined = typeof process !== "undefined" && process.env;
+  //enviroment variables as defined in Node
+  if (envDefined && process.env[key]) return process.env[key];
+  return "";
+}
+
 
 export const queryPatientIdKey = 'launch_queryPatientId';
-
